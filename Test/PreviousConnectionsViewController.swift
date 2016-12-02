9394303e5171c3b7f01fb449fd72742acdf1c24b
John Reyna <jrreyna@cs.stanford.edu>
2016-12-02 12:59:28 -0800

    add call screen with hangup button, John's profile and link all together

---
+++ Test/PreviousConnectionsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PreviousConnectionsViewController.swift
+//  Test
+//
+//  Created by John Reyna on 12/2/16.
+//  Copyright © 2016 John Reyna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PreviousConnectionsViewController: UIViewController {
+    
+}
---
+++ Test/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  Test
+//
+//  Created by John Reyna on 12/2/16.
+//  Copyright © 2016 John Reyna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+}
