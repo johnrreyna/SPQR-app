34f40d3ec2b74d27ca68df7157374534b5c9264b
John Reyna <jrreyna@cs.stanford.edu>
2016-12-02 12:16:46 -0800

    add brian's profile and call button that takes us to call screen

---
+++ Test/CallScreenViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CallScreenViewController.swift
+//  Test
+//
+//  Created by John Reyna on 12/2/16.
+//  Copyright © 2016 John Reyna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CallScreenViewController: UIViewController {
+
+}
