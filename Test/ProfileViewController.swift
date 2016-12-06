5ef9b18f6c5f11bf6cae66fbc5802c634fd0089a
John Reyna <jrreyna@cs.stanford.edu>
2016-12-06 07:24:31 -0800

    add texting screen, add buttons to transition to every screen and back

--- Test/ProfileViewController.swift
+++ Test/ProfileViewController.swift
@@ -11,4 +11,13 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = true
+        
+        /*
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        self.navigationController?.navigationBar.shadowImage = UIImage();
+        self.navigationController?.navigationBar.translucent = true
+        */
+    }
 }
---
+++ Test/TextViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TextViewController.swift
+//  Test
+//
+//  Created by John Reyna on 12/6/16.
+//  Copyright © 2016 John Reyna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextViewController: UIViewController {
+    
+}
