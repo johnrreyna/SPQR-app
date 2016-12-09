40ac596a7afe8a83b14f9f41b83f649003695b21
John Reyna <jrreyna@cs.stanford.edu>
2016-12-08 21:04:11 -0800

    Make backgrounds white

--- Test/TextViewController.swift
+++ Test/TextViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 
 class TextViewController: UIViewController {
     
-    @IBOutlet weak var mapPicture: UIImageView!
     @IBOutlet weak var textScrollView: UIScrollView!
     @IBOutlet weak var roundedMatchPic: UIImageView!
     
@@ -19,13 +18,9 @@ class TextViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textScrollView.contentSize.height = 400;
+        textScrollView.contentSize.height = 1000;
         
         roundedMatchPic.layer.cornerRadius = roundedMatchPic.frame.size.width / 2
         roundedMatchPic.clipsToBounds = true
-        
-        mapPicture.layer.cornerRadius = mapPicture.frame.size.width / 16
-        mapPicture.clipsToBounds = true
-        
     }
 }
