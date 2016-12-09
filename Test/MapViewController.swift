dfa142a6af514b9df306bebe0f321f1fde455ff8
John Reyna <jrreyna@cs.stanford.edu>
2016-12-09 09:16:51 -0800

    Finish transitions, only missing map page 'and it's a match' page

---
+++ Test/MapViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MapViewController.swift
+//  Test
+//
+//  Created by John Reyna on 12/9/16.
+//  Copyright © 2016 John Reyna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var toProfileButton: UIButton!
+    
+    
+    @IBAction func toPreviousPage(_ sender: Any) {
+        let viewControllers: [UIViewController] = self.navigationController!.viewControllers
+        for aViewController in viewControllers {
+            if(aViewController is ProfileViewController){
+                self.navigationController!.popToViewController(aViewController, animated: false);
+            }
+        }
+
+    }
+}
